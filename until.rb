a40382f740a844754e05f428bf2535b89e8dc344
tiffanyyoon <yyoon032@rsdmo.org>
2018-06-19 19:31:41 +0000

    automatically backed up by learn

--- until.rb
+++ until.rb
@@ -1,3 +1,10 @@
-def until 
+def using_untiluntil 
   counter = 0 
-  
+   until counter < 10 
+    puts "Wingardium Leviosa"
+    puts ""
+  counter = counter + 1
+    puts "Level: #{counter}"
+  end 
+end 
+using_until
